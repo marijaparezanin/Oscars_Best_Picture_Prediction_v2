46e86a44cdc74b9f5089ad08c76861217100d2f8
marijaparezanin <marija.parezanin@mensa.ba>
2025-03-01 21:40:53 +0100

    [FIX] - Try to fix CORS issue

--- oscar_web_app.py
+++ oscar_web_app.py
@@ -7,6 +7,17 @@ app = Flask(__name__)
 
 CORS(app, resources={r"/predict": {"origins": "https://marijaparezanin.github.io"}})
 
+
+@app.before_request
+def before_request():
+    # Manually handle preflight OPTIONS requests
+    if request.method == 'OPTIONS':
+        response = app.make_response('')
+        response.headers['Access-Control-Allow-Origin'] = 'https://marijaparezanin.github.io'
+        response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        return response
+
 @app.route('/')
 def index():
     # Render the main HTML template
