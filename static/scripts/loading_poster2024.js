474fbce5ccaf1bbe2c3cdb37241458d14e0bb62c
marijaparezanin <marija.parezanin@mensa.ba>
2025-03-01 21:29:53 +0100

    [ADD] - Add more adjustments to static paths for fonts and posters

--- static/scripts/loading_poster2024.js
+++ static/scripts/loading_poster2024.js
@@ -21,7 +21,7 @@ const parseCSV = (csv) => {
         var name = values[0].trim()
         var year = values[3].trim()
         movie["Title"] = name;
-        movie["Image"] = "/static/posters/"+name+".jpg"
+        movie["Image"] = "./static/posters/"+name+".jpg"
         movie["Alt"] = name + " ("  + year + ")";
     
         movies.push(movie);
